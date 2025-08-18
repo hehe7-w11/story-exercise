77da5f145e15f0ff51e15bf94c132146d949ab13
weihuang <thoughtworks.ht.huang@mbusa.com>
2025-08-18 15:53:41 +0800

    add account transfer demo

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,20 +1,2 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class AccountTest {
-
-    @Test
-    void shouldCreateAccountWithValidIdAndBalance() {
-        // given
-        String id = "A1";
-        double initialBalance = 100.0;
-
-        // when
-        Account account = new Account(id, initialBalance);
-
-        // then
-        assertEquals("A1", account.getId());
-        assertEquals(100.0, account.getBalance());
-    }
 }
