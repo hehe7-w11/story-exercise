32ced73cc1b527cf8611e2ffcbfead50cdc14152
HEHE7 <hehe7@oocl.com>
2025-08-25 16:48:51 +0800

    feat: 增加支持clever parkingBoy在多个parkingLot停车的功能

--- src/main/java/Story1/ParkingBoy.java
+++ src/main/java/Story1/ParkingBoy.java
@@ -2,11 +2,14 @@ package Story1;
 
 import UserDefinedException.NoAvaialblePositionException;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class ParkingBoy {
 
     private List<ParkingLot> parkingLotList;
+    private Boolean isClever;
 
     public List<ParkingLot> getParkingLotList() {
         return parkingLotList;
@@ -16,15 +19,37 @@ public class ParkingBoy {
         this.parkingLotList = parkingLotList;
     }
 
-    public ParkingBoy(List<ParkingLot> parkingLotList) {
-        this.parkingLotList = parkingLotList;
+
+    public Boolean getClever() {
+        return isClever;
+    }
+
+    public void setClever(Boolean clever) {
+        isClever = clever;
     }
 
 
-    public Ticket park(List<ParkingLot> parkingLotList, Car car) throws NoAvaialblePositionException {
-        for(ParkingLot parkingLot : parkingLotList){
-            if(parkingLot.getCapacity() > 0){
-               return parkingLot.park(car);
+    public ParkingBoy(List<ParkingLot> parkingLotList, Boolean isClever) {
+        this.parkingLotList = parkingLotList;
+        this.isClever = isClever;
+    }
+
+
+    public Ticket park(List<ParkingLot> parkingLotList, Car car, Boolean isClever) throws NoAvaialblePositionException {
+        if (!isClever) {
+            for (ParkingLot parkingLot : parkingLotList) {
+                if (parkingLot.getCapacity() > 0) {
+                    return parkingLot.park(car);
+                }
+            }
+        } else {
+            Optional<ParkingLot> optionalParkingLot = parkingLotList.stream()
+                    .max(Comparator.comparingInt(ParkingLot::getCapacity));
+            if (optionalParkingLot.isPresent()) {
+                ParkingLot parkingLot = optionalParkingLot.get();
+                if (parkingLot.getCapacity() > 0) {
+                    return parkingLot.park(car);
+                }
             }
         }
         return null;
@@ -32,8 +57,8 @@ public class ParkingBoy {
 
 
     public Car fetch(Ticket ticket, List<ParkingLot> parkingLotList) {
-        for(ParkingLot parkingLot : parkingLotList){
-            if(parkingLot.getCarSet().contains(ticket.getCar().getId())){
+        for (ParkingLot parkingLot : parkingLotList) {
+            if (parkingLot.getCarSet().contains(ticket.getCar().getId())) {
                 return parkingLot.fetch(ticket, parkingLot);
             }
         }
